68daf426c671cc87a3944559e479c99b8b10c132
aishakandeell <aishakandeel@aucegypt.edu>
2024-04-24 22:37:00 +0200

    hi

--- ball.h
+++ ball.h
@@ -1,10 +1,19 @@
 #ifndef BALL_H
 #define BALL_H
+#include <QGraphicsRectItem>
+#include <QObject>
 
-class Ball
-{
+class Ball:  public QObject, public QGraphicsRectItem{
+    Q_OBJECT
 public:
-    Ball();
+    Ball(QGraphicsItem* parent=nullptr);
+    void reverseball();
+
+public slots:
+    void move();
+private:
+    double moveX;
+    double moveY;
 };
 
 #endif // BALL_H
